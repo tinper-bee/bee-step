ca9265767e74f735d8978ff5de17492df395a753
“jonyshi” <jonyshi@126.com>
2019-03-01 10:06:55 +0800

    publish 2.0.0

--- build/utils.js
+++ build/utils.js
@@ -4,6 +4,10 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.isFlexSupported = isFlexSupported;
+/**
+* This source code is quoted from rc-steps.
+* homepage: https://github.com/react-component/steps
+*/
 function isFlexSupported() {
   if (typeof window !== 'undefined' && window.document && window.document.documentElement) {
     var documentElement = window.document.documentElement;
